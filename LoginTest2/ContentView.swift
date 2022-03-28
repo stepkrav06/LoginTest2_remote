87ce34ae7ec4d9fb3f0426cd9b38e30305cac821
Stepan Kravtsov <dfg>
2022-03-28 22:53:40 +0300

    Initial Commit

---
+++ LoginTest2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginTest2
+//
+//  Created by Степан Кравцов on 28.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginTest2/LoginTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTest2App.swift
+//  LoginTest2
+//
+//  Created by Степан Кравцов on 28.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
